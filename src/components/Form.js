2047d0c0305cb6ecf66e14992f693f16c4a6b73a
malla-lokesh <121810307046@gitam.in>
2023-04-06 13:07:55 +0530

    project completed

--- src/components/Form.js
+++ src/components/Form.js
@@ -8,13 +8,26 @@ const Form = () => {
     const [age, setAge] = useState('');
     const [output, setOutput] = useState([]);
     const [key, setKey] = useState(0);
+    const [errorMsg, setErrorMsg] = useState('');
 
     const formHandler = (e) => {
         e.preventDefault();
+        if(username.trim().length===0) {
+            setErrorMsg('Please enter a valid username.');
+            return;
+        }
+        if(age.trim().length===0 || +age < 1) {
+            setErrorMsg('Age must be above 1.');
+            return;
+        }
         setKey(key + 1);
         setOutput([...output, <Output key={key} username={username} age={age}/>])
         setUsername('');
         setAge('');
+    }
+
+    const toggleError = () => {
+        setErrorMsg('');
     }
 
     return (
@@ -23,10 +36,10 @@ const Form = () => {
                 <div className="col-3"></div>
                 <div className="col form">
                     <form onSubmit={formHandler}>
-                        <label className="form-label" for="username">Username</label>
+                        <label className="form-label" htmlFor="username">Username</label>
                         <input className="form-control input" id="username" type="text" value={username} onChange={e => setUsername(e.target.value)}></input>
                         <br/>
-                        <label className="form-label" for="age">Age</label>
+                        <label className="form-label" htmlFor="age">Age</label>
                         <input className="form-control input" id="age" type="text" value={age} onChange={e => setAge(e.target.value)}></input>
                         <br/>
                         <button className="btn btn-primary" type="submit">Add User</button>
@@ -34,6 +47,14 @@ const Form = () => {
                 </div>
                 <div className="col-3"></div>
             </div>
+            {errorMsg && (
+                <div className="overlay active" onClick={toggleError}>
+                    <div className="overlay-content">
+                        <p>{errorMsg}</p>
+                        <button className="btn btn-warning" onClick={() => setErrorMsg('')}>Ok</button>
+                    </div>
+                </div>
+            )}
             <div className="row pt-4">
                 <div className="col-3"></div>
                 <div className="col">
